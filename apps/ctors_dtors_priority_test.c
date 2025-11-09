8a696ee5d9aa1dcad7772a7b7e9f9a9ac9862c51
Fabio Porcedda <fabio.porcedda@gmail.com>
2025-11-09 18:09:12 +0100

    Fix -Wstrict-prototypes warnings
    Functions without a prototype are deprecated so fix related warnings:
    
    gnu-efi//apps/../inc/efiapi.h:757:5: warning: function declaration isn’t a prototype [-Wstrict-prototypes]
      757 |     );
          |     ^
    
    gnu-efi//apps/ctors_dtors_priority_test.c:5:64: warning: function declaration isn’t a prototype [-Wstrict-prototypes]
        5 | static void __attribute__((constructor(101))) EFI_NO_TAIL_CALL ctors101() {
          |                                                                ^~~~~~~~
    gnu-efi//apps/ctors_dtors_priority_test.c:10:66: warning: function declaration isn’t a prototype [-Wstrict-prototypes]
       10 | static void __attribute__((constructor(65434))) EFI_NO_TAIL_CALL ctors65434() {
          |                                                                  ^~~~~~~~~~
    gnu-efi//apps/ctors_dtors_priority_test.c:15:63: warning: function declaration isn’t a prototype [-Wstrict-prototypes]
       15 | static void __attribute__((destructor(101))) EFI_NO_TAIL_CALL dtors101() {
          |                                                               ^~~~~~~~
    gnu-efi//apps/ctors_dtors_priority_test.c:20:65: warning: function declaration isn’t a prototype [-Wstrict-prototypes]
       20 | static void __attribute__((destructor(65434))) EFI_NO_TAIL_CALL dtors65434() {
          |                                                                 ^~~~~~~~~~
    
    Signed-off-by: Fabio Porcedda <fabio.porcedda@gmail.com>

--- apps/ctors_dtors_priority_test.c
+++ apps/ctors_dtors_priority_test.c
@@ -2,22 +2,22 @@
 #include <efilib.h>
 
 // 101 in init_array, 65434 in ctors
-static void __attribute__((constructor(101))) EFI_NO_TAIL_CALL ctors101() {
+static void __attribute__((constructor(101))) EFI_NO_TAIL_CALL ctors101(void) {
     Print(u"1) ctor with lower numbered priority \r\n");
 }
 
 // 65434 in init_array, 101 in ctors
-static void __attribute__((constructor(65434))) EFI_NO_TAIL_CALL ctors65434() {
+static void __attribute__((constructor(65434))) EFI_NO_TAIL_CALL ctors65434(void) {
     Print(u"2) ctor with higher numbered priority \r\n");
 }
 
 // 101 in fini_array, 65434 in dtors
-static void __attribute__((destructor(101))) EFI_NO_TAIL_CALL dtors101() {
+static void __attribute__((destructor(101))) EFI_NO_TAIL_CALL dtors101(void) {
     Print(u"4) dtor with lower numbered priority \r\n");
 }
 
 // 65434 in fini_array, 101 in dtors
-static void __attribute__((destructor(65434))) EFI_NO_TAIL_CALL dtors65434() {
+static void __attribute__((destructor(65434))) EFI_NO_TAIL_CALL dtors65434(void) {
     Print(u"3) dtor with higher numbered priority \r\n");
 }
 
